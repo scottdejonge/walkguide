2ea205ad2fa9302755001e72e4ea13211f688d82
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-03 21:05:04 +1000

    Index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title>Walk Guide</title>
+	<link rel="stylesheet" type="text/css" href="/generated/styles/styles.css" />
+</head>
+<body>
+	<h1>Walk Guide</h1>
+	<script type="text/javascript" src="/generated/scripts/scripts.js"></script>
+</body>
+</html>
