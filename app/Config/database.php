9e94327cbb86eae5a9c647b887a196cfcc5d422b
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-03 23:29:02 +1000

    Installed CakePHP

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'walkguide',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+}
