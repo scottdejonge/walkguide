2bded8e43203727f447156f8c1fd6ad983a8f5b1
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-03 21:41:43 +1000

    Init basic theme

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +8,65 @@
 	<link rel="stylesheet" type="text/css" href="/generated/styles/styles.css" />
 </head>
 <body>
-	<h1>Walk Guide</h1>
+
+	<header class="header">
+		<div class="container">
+			<div class="header-content">
+				<h1 class="header-title">Walk Name</h1>
+				<h2 class="header-subtitle">National Park</h2>
+			</div>
+			<div class="header-controls">
+				<div class="button-group">
+					<button>Favourite</button>
+					<button>Share</button>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section class="map">
+
+	</section>
+	
+	<section class="tabs">
+		<div class="container">
+			<div class="button-group-block">
+				<div class="button-group">
+					<button>All</button>
+				</div>
+				<div class="button-group">
+					<button>Ratings</button>
+				</div>
+				<div class="button-group">
+					<button>Comments</button>
+				</div>
+				<div class="button-group">
+					<button>Photos</button>
+				</div>
+				<div class="button-group">
+					<button>Videos</button>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="posts">
+		<article class="post">
+			<header class="post-header">
+				<!-- <div class="media media-1">
+					<img class="circle" src="">
+				</div> -->
+				<time>1st July</time>
+			</header>
+			<div class="post-content">
+				
+			</div>
+			<footer class="post-footer">
+				
+			</footer>
+		</article>
+	</section>
+
 	<script type="text/javascript" src="/generated/scripts/scripts.js"></script>
 </body>
 </html>
