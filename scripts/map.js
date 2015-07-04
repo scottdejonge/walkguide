f3ab578e74c97c866ffde1fe2cbf0c2295cb11da
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-04 16:45:19 +1000

    Map styles and polyline

--- scripts/map.js
+++ scripts/map.js
@@ -10,15 +10,14 @@ var $ = require('jquery');
  */
 
 var map;
-var $map = $('[data-map]').get(0);
+var $map = $('[data-map]');
+var mapStyles = [{"featureType":"water","elementType":"all","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"on"}]},{"featureType":"water","elementType":"labels","stylers":[{"hue":"#7fc8ed"},{"saturation":55},{"lightness":-6},{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"hue":"#83cead"},{"saturation":1},{"lightness":-15},{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"hue":"#f3f4f4"},{"saturation":-84},{"lightness":59},{"visibility":"on"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbbbbb"},{"saturation":-100},{"lightness":26},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-35},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffcc00"},{"saturation":100},{"lightness":-22},{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"hue":"#d7e4e4"},{"saturation":-60},{"lightness":23},{"visibility":"on"}]}];
 var mapOptions =  {
 	zoom: 8,
-	scrollwheel: false,
-	center: {
-		lat: -34.397,
-		lng: 150.644
-	}
+	styles: mapStyles,
+	scrollwheel: false
 };
+
 
 
 /*
@@ -34,7 +33,16 @@ function initialise() {
  */
 
 function initialiseMap() {
-	map = new google.maps.Map($map, mapOptions);
+	var polyline = $map.data('map-polyline');
+	console.log(polyline);
+
+	// Create Map
+	map = new google.maps.Map($map.get(0), mapOptions);
+
+	var polylineLayer = new google.maps.KmlLayer({
+		url: 'http://gmaps-samples.googlecode.com/svn/trunk/ggeoxml/cta.kml'
+	});
+	polylineLayer.setMap(map);
 }
 
 
