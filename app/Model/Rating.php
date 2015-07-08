f34410737781a9fe6f94fe0ba6d8608e841eafb3
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-08 16:01:27 +1000

    Walks average rating save callback

--- app/Model/Rating.php
+++ app/Model/Rating.php
@@ -2,4 +2,21 @@
 
 class Rating extends AppModel {
 	
+	/**
+	 * Save Callbacks
+	 */
+		
+	public function updateAllAverageRatings() {
+		$sql = "UPDATE ratings c
+				SET average_rating = (
+					SELECT COUNT(projects.id)
+					FROM commodities_projects
+						LEFT JOIN projects ON commodities_projects.project_id = projects.id
+					WHERE
+						commodities_projects.commodity_id = c.id AND
+						projects.published = 1 AND
+						projects.archived = 0
+				)";
+		$this->query($sql);
+	}
 }
--- app/Model/Walk.php
+++ app/Model/Walk.php
@@ -2,8 +2,22 @@
 
 class Walk extends AppModel {
 
+
+	/**
+	 * Relationships
+	 */
+
 	public $hasMany = array(
 		'Comment',
 		'Rating',
 	);
+
+
+	/**
+	 * Save Callbacks
+	 */
+
+	public function afterSaveAll($result) {
+		//$this->Rating->updateAllAverageRatings();
+	}
 }
