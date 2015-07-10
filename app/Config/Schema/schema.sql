ee1899dfb1f60c49b6d80535c7e445850ab6ed3d
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-10 16:05:08 +1000

    Schema updated

--- app/Config/Schema/schema.sql
+++ app/Config/Schema/schema.sql
@@ -3,19 +3,61 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50622
+ Source Server Version : 50619
  Source Host           : localhost
  Source Database       : walkguide
 
  Target Server Type    : MySQL
- Target Server Version : 50622
+ Target Server Version : 50619
  File Encoding         : utf-8
 
- Date: 07/04/2015 13:34:47 PM
+ Date: 07/10/2015 16:04:58 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `comments`
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `walk_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comment` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+--  Table structure for `ratings`
+-- ----------------------------
+DROP TABLE IF EXISTS `ratings`;
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `walk_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `rating` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=268 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `walks`
@@ -42,12 +84,14 @@ CREATE TABLE `walks` (
   `wheelchair` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `fireline` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `material` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `average_rating` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
   `geometry` longtext COLLATE utf8_unicode_ci,
   `start_lat` int(11) DEFAULT NULL,
   `start_lng` int(11) DEFAULT NULL,
   `end_lat` int(11) DEFAULT NULL,
   `end_lng` int(11) DEFAULT NULL,
+  `featured` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2143 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2143 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 SET FOREIGN_KEY_CHECKS = 1;
